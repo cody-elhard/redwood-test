babf26c4a8938e427cb3b5275dc0de175525e392
Cody Elhard <32208000+cody-elhard@users.noreply.github.com>
2022-06-06 16:37:11 -0500

    Add a couple pages with layouts

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,17 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <h1>About page</h1>
+      <p>
+        This site was created to demonstrate the superpowers of redwood by
+        previous GitHub co-founder
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
